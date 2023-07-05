e1b0732b606a9d1730a7938d84bcb1f1eedf88b7
nomozovasror <nomozovasroe@gmail.com>
2023-07-05 17:03:06 +0500

    info screen text field add

--- lib/screens/info.dart
+++ lib/screens/info.dart
@@ -2,6 +2,8 @@
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:like_button/like_button.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -17,19 +19,23 @@ class InfoScreen extends StatefulWidget {
 }
 
 class _InfoScreenState extends State<InfoScreen> {
-
   int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
+    bool isLiked = false;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           leading: GestureDetector(
-            child: const Icon( Icons.arrow_back_ios, color: Colors.white, size: 20, ),
+            child: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+              size: 20,
+            ),
             onTap: () {
               Navigator.pop(context);
-            } ,
-          ) ,
+            },
+          ),
           title: Image.asset('assets/images/logo.png', height: 40),
           backgroundColor: const Color(0xff008B51),
           centerTitle: true,
@@ -46,7 +52,8 @@ class _InfoScreenState extends State<InfoScreen> {
                       onPageChanged: (index, reason) =>
                           setState(() => activeIndex = index)),
                   itemCount: widget.house.img.length,
-                  itemBuilder: (BuildContext context, int index, int realIndex) {
+                  itemBuilder:
+                      (BuildContext context, int index, int realIndex) {
                     final imgL = widget.house.img[index];
                     return Container(
                       decoration: BoxDecoration(
@@ -68,6 +75,193 @@ class _InfoScreenState extends State<InfoScreen> {
                           dotColor: Colors.white),
                     ))
               ]),
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text("06.06.2023",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff666666))),
+                    const Row(
+                      children: [
+                        Icon(
+                          Icons.remove_red_eye_outlined,
+                          size: 16,
+                          color: Color(0xff666666),
+                        ),
+                        Text("223",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xff666666))),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding:
+                              const EdgeInsets.only(right: 8, left: 12, top: 1),
+                          decoration: const BoxDecoration(
+                              color: Color(0xff008B51),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8),
+                                bottomLeft: Radius.circular(8),
+                              )),
+                          child: LikeButton(
+                            size: 24,
+                            isLiked: isLiked,
+                            likeBuilder: (isTapped) {
+                              return SvgPicture.asset(
+                                'assets/icons/mdi_heart-outline.svg',
+                                color: isTapped
+                                    ? Colors.red
+                                    : const Color(0xffffffff),
+                              );
+                            },
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.only(
+                                top: 1, right: 10, left: 10),
+                            decoration: const BoxDecoration(
+                                color: Color(0xff008B51),
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(8),
+                                  bottomRight: Radius.circular(8),
+                                )),
+                            child: SvgPicture.asset(
+                              "assets/icons/system-uicons_forward.svg",
+                              color: Colors.white,
+                              width: 24,
+                            ))
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const Row(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text("2 250 000 so’m",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xff008B51))),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, top: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Olmazor tumanida joylashgan 2x kvartira ijaraga beriladi",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, top: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Сдается в аренду для семьи трёхкомнатная квартира в центральном, спальном районе,на Дархане. Доброжелательные соседи,развитая инфраструктура,в трёх минутах от метро Хамид Олимжан. Рядом  есть школа,детский сад.Квартира полностью оснащена для жилья: свежий ремонт, меблирована, детская будет обставлена по желанию жильцов,три телевизора,два кондиционера, большой холодильник, стиральная машина, пылесос. Техника, мебель и посуда новые,не пользованные. Вы будете первым хозяином. ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 12,
+                            color: Color(0xff666666)),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  maxLines: 8,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          )),
+                      filled: true,
+                      fillColor: const Color(0xffF1F1F1),
+                      hintText: "Uy egasiga yozish",
+                      hintStyle: const TextStyle(
+                          color: Color(0xffABABAB), fontSize: 14)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          launch("tel:+998919998877");
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xff008B51),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 6),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.phone_rounded,
+                              size: 20,
+                            ),
+                            Text(
+                              " Qo'ng'iroq qilish",
+                              style: TextStyle(fontSize: 14),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 40,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xffFF8D08),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 6),
+                        ),
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Yuborish ",
+                              style: TextStyle(fontSize: 14),
+                            ),
+                            Icon(
+                              Icons.send_outlined,
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
